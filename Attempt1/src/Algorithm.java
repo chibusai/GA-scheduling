4d8ab3cfaa36dc9856b0cd916bdbfb73f76c36fc
chibusai <chi_trinh@mymail.sutd.edu.sg>
2018-04-11 12:25:05 +0800

    added comments

--- Attempt1/src/Algorithm.java
+++ Attempt1/src/Algorithm.java
@@ -7,6 +7,18 @@
  * Fitter individuals are given a higher chance to mate and yield more "fitter" individuals
  * We keep "evolving" better individuals or solutions over generations until we reach a terminating criterion.
  * GA is randomized but performs better than random local search as they exploit historical information as well.
+ *
+ * GA()
+ *      initialize population
+ *      find fitness of population
+ *      while (termination criteria is reached) do
+ *          parent selection
+ *          crossover with probability pc
+ *          mutation with probability pm
+ *          decode and fitness calculation
+ *          survivor selection
+ *          find best
+ *      return best
  */
 
 public class Algorithm {
