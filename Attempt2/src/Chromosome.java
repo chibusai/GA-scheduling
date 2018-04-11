25e3f1dbd602d3d10af78287e7db3f713f6ac31a
chibusai <chi_trinh@mymail.sutd.edu.sg>
2018-04-11 12:53:35 +0800

    added comments

--- Attempt2/src/Chromosome.java
+++ Attempt2/src/Chromosome.java
@@ -1,11 +1,17 @@
 /**
  * Each chromosome is an array of arrays of integers.
- * Each array is 
+ *
+ * Each inner array represents a gene, containing subject, session, student group,
+ * instructor, duration, location and start time.
  */
 
 public class Chromosome {
 
-    /**
-     *
-     */
+    private int[] chromosome;
+    private double fitness;
+
+    public Chromosome(Schedule schedule) {
+
+        
+    }
 }
