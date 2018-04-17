76c33259a70434f49e0b39b3b0927149e4110bdb
chibusai <chi_trinh@mymail.sutd.edu.sg>
2018-04-18 03:07:05 +0800

    updated various classes

--- Attempt2/src/Class.java
+++ Attempt2/src/Class.java
@@ -1,4 +1,64 @@
 public class Class {
+
+    /**
+     * fixed parameters
+     */
     private final int classId;
-    private
+    private final int sessionId;
+    private final int studentGroupId;
+    private final int professorId;
+    private final int durationId;
+
+    /**
+     * variable parameters
+     */
+    private int venueId;
+    private int startTimeId;
+
+    public Class(int classId, int sessionId, int studentGroupId, int professorId, int durationId, int venueId, int startTimeId) {
+        this.classId = classId;
+        this.sessionId = sessionId;
+        this.studentGroupId = studentGroupId;
+        this.professorId = professorId;
+        this.durationId = durationId;
+        this.venueId = venueId;
+        this.startTimeId = startTimeId;
+    }
+
+
+    public int getClassId() {
+        return classId;
+    }
+
+    public int getSessionId() {
+        return sessionId;
+    }
+
+    public int getStudentGroupId() {
+        return studentGroupId;
+    }
+
+    public int getProfessorId() {
+        return professorId;
+    }
+
+    public int getDurationId() {
+        return durationId;
+    }
+    
+    public int getVenueId() {
+        return venueId;
+    }
+
+    public void setVenueId(int venueId) {
+        this.venueId = venueId;
+    }
+
+    public int getStartTimeId() {
+        return startTimeId;
+    }
+
+    public void setStartTimeId(int startTimeId) {
+        this.startTimeId = startTimeId;
+    }
 }
--- Attempt2/src/VenueType.java
+++ Attempt2/src/VenueType.java
@@ -1,2 +1,6 @@
 public enum VenueType {
+    THINK_TANK,
+    COHORT_CLASSROOM,
+    LECTURE_THEATRE,
+    LAB
 }
